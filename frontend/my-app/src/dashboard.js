f22e793b962977cf5358f5a81cea64e862c0d2cd
Pawan <pawan99@iastate.edu>
2022-05-05 23:28:36 -0500

    auth updtae

---
+++ frontend/my-app/src/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AlertDialog from "./alertDialog";
+  
+const Dashboard = () => {
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <AlertDialog></AlertDialog>
+    </div>
+   
+    
+  );
+};
+  
+export default Dashboard;
