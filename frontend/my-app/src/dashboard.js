ea6ef9671b2a93969aa5d08bc30f28e20ad6e2c9
Pawan <pawan99@iastate.edu>
2022-05-08 22:22:59 -0500

    ec2 update

--- frontend/my-app/src/dashboard.js
+++ frontend/my-app/src/dashboard.js
@@ -3,6 +3,33 @@ import AlertDialog from "./alertDialog";
   
 const Dashboard = () => {
 
+    const axios = require('axios');
+
+    let req = {
+        "token1": "gZMlivUH5RbUu/shSJvkKeZlKbg2mdjuyP5n6t1X+xI=",
+        "token2": "ZJaGxaR+HPmdJkTGEvXFEkOgizQM4IqM9WcXu02Ylid8D1G3IDZYgGC/hgSz4DNo",
+        "username": "Dharma"
+    }
+    
+      axios.post('http://localhost:8080/home/s3/getBucketList', req)
+          .then(function (response) {
+            console.log(response);
+      
+            
+    
+            console.log('Response Code: '+response.status);
+            
+           
+            
+          
+            
+          })
+          .catch(function (error) {
+            
+            alert('Invalid Credentials');
+            console.log(error);
+          });
+
   return (
     <div>
       <h1>Dashboard</h1>
